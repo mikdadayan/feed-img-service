6936322ea3c3f948ad2ee23da1a4d122e7775ed6
mikdadayan <dadayan.mikayel@gmail.com>
2023-05-31 20:25:42 +0400

    fix: update schema, error handling on controllers.

--- src/lib/controllers/index.ts
+++ src/lib/controllers/index.ts
@@ -7,57 +7,82 @@ import { createSuccessResponse } from "../utils/response-utils";
 import sharp from "sharp";
 import { randomImageName } from "../utils/util-fucntions";
 import { S3CommandType, createS3Command } from "../facade/createS3Command";
+import CustomError from "../utils/customError";
 
-export const getAllPosts = async (_req: Request, res: Response) => {
-  const posts = await prisma.post.findMany({
-    orderBy: [{ createdAt: "desc" }],
-  });
-  for (const post of posts) {
-    const command = createS3Command(S3CommandType.GET, {
-      Bucket: bucketName,
-      Key: post.imageName,
+export const getAllPosts = async (req: Request, res: Response) => {
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: [{ createdAt: "desc" }],
     });
-    const url = await getSignedUrl(s3, command, { expiresIn: 60 * 60 });
-    post.imageUrl = url;
+
+    for (const post of posts) {
+      const command = createS3Command(S3CommandType.GET, {
+        Bucket: bucketName,
+        Key: post.imageName,
+      });
+      const url = await getSignedUrl(s3, command, { expiresIn: 60 * 60 });
+      post.imageUrl = url;
+    }
+
+    return createSuccessResponse(res, "List of all posts.", 200, posts);
+  } catch (error) {
+    throw new CustomError("Failed to fetch posts", 500);
   }
-  return createSuccessResponse(res, "List of all posts.", 200, posts);
 };
 
 export const createPost = async (req: Request, res: Response) => {
-  const buffer = await sharp(req.file?.buffer)
-    .resize({ height: 1920, width: 1080, fit: "contain" })
-    .toBuffer();
-  const imageName = randomImageName();
-  const command = createS3Command(S3CommandType.PUT, {
-    Bucket: bucketName,
-    Key: imageName,
-    Body: buffer,
-    ContentType: req.file?.mimetype,
-  });
-  await s3.send(command);
-  // Create the post metadata in the database
-  const post = await prisma.post.create({
-    data: {
-      caption: req.body.caption || "",
-      imageName: String(imageName),
-    },
-  });
+  try {
+    if (!req.file) {
+      throw new CustomError("Image file is required", 400);
+    }
 
-  return createSuccessResponse(res, "Created post successfully.", 201, post);
+    const buffer = await sharp(req.file.buffer)
+      .resize({ height: 1920, width: 1080, fit: "contain" })
+      .toBuffer();
+
+    const imageName = randomImageName();
+    const command = createS3Command(S3CommandType.PUT, {
+      Bucket: bucketName,
+      Key: imageName,
+      Body: buffer,
+      ContentType: req.file.mimetype,
+    });
+
+    await s3.send(command);
+
+    const post = await prisma.post.create({
+      data: {
+        caption: req.body.caption || "",
+        imageName: String(imageName),
+      },
+    });
+
+    return createSuccessResponse(res, "Created post successfully.", 201, post);
+  } catch (error) {
+    throw new CustomError("Failed to create post", 500);
+  }
 };
 
 export const deletePost = async (req: Request, res: Response) => {
-  const id = +req.params.id;
-  const post = await prisma.posts.findUnique({ where: { id } });
-  if (!post) {
-    res.status(404).send("Post not found");
-    return;
+  try {
+    const id = req.params.id;
+    const post = await prisma.post.findUnique({ where: { id } });
+
+    if (!post) {
+      throw new CustomError("Post not found", 404);
+    }
+
+    const command = createS3Command(S3CommandType.DELETE, {
+      Bucket: bucketName,
+      Key: post.imageName,
+    });
+
+    await s3.send(command);
+
+    await prisma.post.delete({ where: { id } });
+
+    return createSuccessResponse(res, "Post deleted successfully.", 204, {});
+  } catch (error) {
+    throw new CustomError("Failed to delete post", 500);
   }
-  const command = createS3Command(S3CommandType.DELETE, {
-    Bucket: bucketName,
-    Key: post.imageName,
-  });
-  await s3.send(command);
-  await prisma.posts.delete({ where: { id } });
-  return createSuccessResponse(res, "Post deleted successfuly.", 204, {});
 };
